4d137feda60b80b3d0faadcb0c81350ad8266b29
apdonaghy <adonaghy@seattleu.edu>
2020-02-16 00:47:27 +0000

    set up config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-$API_KEY = 'Put your API key here'; // This will be your Flickr-issued API Key.
+$API_KEY = 'b3c6ab1a3d726cc32081c808277ccfeb'; // This will be your Flickr-issued API Key.
 $PAGE_TITLE = 'Kittens and Rainbows'; // This should be a user-friendly page title.
 $SEARCH_TAGS = 'kitten,rainbow'; // This must be a comma-separated list
 
